b282ac65c6f2863cbc88ae744750cbb60a1aa5e7
Viniciusmendes21 <viniciusmendes21@hotmail.com>
2024-10-28 09:00:18 -0300

    adicionado controller tipofeedbackproduto

--- SampleWebApi/Program.cs
+++ SampleWebApi/Program.cs
@@ -9,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddScoped<IFeedbackProdutoRepository, FeedbackProdutoRepository>();
 builder.Services.AddScoped<IPerfilRepository, PerfilRepository>();
+builder.Services.AddScoped<ITipoFeedbackProdutoRepository, TipoFeedbackProdutoRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
